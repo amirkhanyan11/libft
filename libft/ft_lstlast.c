2bec0a8aeec6e7c8e5d1bc632b754367b7252724
kali <kali@kali.com>
2024-01-30 01:54:14 +0400

    added rules to makefile

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 19:44:58 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/01/29 20:09:06 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/01/30 01:53:32 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 static t_list	*_get_lstlast(t_list *lst);
-static int		_cycle_detect(t_list *lst);
+static int		_cycle_catch(t_list *lst);
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	if (!lst || _cycle_detect(lst))
+	if (!lst || _cycle_catch(lst))
 		return (NULL);
 	return (_get_lstlast(lst));
 }
@@ -29,7 +29,7 @@ static t_list	*_get_lstlast(t_list *lst)
 	return (_get_lstlast(lst->next));
 }
 
-static int	_cycle_detect(t_list *lst)
+static int	_cycle_catch(t_list *lst)
 {
 	t_list	*s;
 	t_list	*f;
