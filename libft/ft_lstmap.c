adf3f87b299d97e7ec8658b7751e85666a72a5d0
Artyom Amirkhanyan <aamirkha@c2r6s3.42yerevan.am>
2024-01-29 17:18:58 +0400

    ui

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,22 +6,35 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 20:28:28 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/01/27 20:38:30 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:17:38 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "_bonus.h"
 
-static t_list	*_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+static void	_lstmap(t_list *other, t_list **head, void *(*f)(void *), void (*del)(void *));
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	//if (lst && f && del)
-	t_list	*head = ft_lstnew()
+	t_list	*head;
+
+	head = NULL;
+	if (!lst || !f || !del)
+		return (NULL);
+	_lstmap(lst, &head, f, del);
+	return (head);
 }
 
-static t_list	*_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+static void	_lstmap(t_list *other, t_list **head, void *(*f)(void *), void (*del)(void *))
 {
-	if (!lst)
-		return (NULL);
+	if (!other)
+		return ;
+	*head = ft_lstnew(f(other->content));
+	if (!head)
+	{
+		ft_lstclear(head, del);
+		return ;
+	}
+	_lstmap(other->next, &((*head)->next), f, del);
 }
+
